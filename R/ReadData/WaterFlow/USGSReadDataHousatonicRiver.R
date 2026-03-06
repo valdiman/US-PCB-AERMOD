5b870da45b8a153745f2c5e08013de87c0a61b83
martie <andres-martinez@uiowa.edu>
2026-03-06 15:38:16 -0600

    New version.

---
+++ R/ReadData/WaterFlow/USGSReadDataHousatonicRiver.R
@@ -0,0 +1,84 @@
+# -------------------------------------------------------------------
+# Code use to retrieve water flow (Q) from a USGS station
+
+# Install packages
+{
+  install.packages("dataRetrieval")
+  install.packages("dplyr")
+  install.packages('sf')
+  install.packages("zoo")
+  install.packages("lubridate")
+}
+
+# Load libraries
+{
+  library(dataRetrieval) # read data from USGS
+  library(dplyr)         # data manipulation
+  library(sf)            # for handling sf objects (used by read_waterdata_daily)
+  library(zoo)           # for interpolation
+  library(lubridate)
+  library(geosphere)
+}
+
+# Read data ---------------------------------------------------------------
+# From Data Folder, Housatonic River.csv
+hor <- read.csv("Data/HousatonicRiver/HousatonicRiver_env.csv")
+
+# Convert from "m/d/YYYY" to Date
+hor$SampleDate <- as.Date(hor$SampleDate)
+
+# Seek stations
+sites <- read_waterdata_monitoring_location(
+  bbox = c(-73.45, 42.25, -73.05, 42.65))
+
+# replace with your target point (lon, lat)
+target_lon <- -73.2454
+target_lat <- 42.4501
+
+# extract coords from sf geometry
+coords <- st_coordinates(sites)
+sites_near_5km <- sites %>%
+  st_drop_geometry() %>%
+  mutate(
+    lon = coords[,1],
+    lat = coords[,2],
+    dist_km = distHaversine(
+      cbind(lon, lat),
+      cbind(target_lon, target_lat)
+    ) / 1000
+  ) %>%
+  filter(dist_km <= 5) %>%
+  arrange(dist_km) %>%
+  select(monitoring_location_id, monitoring_location_name, lat, lon, dist_km)
+
+# Define USGS site and parameter ------------------------------------------
+site <- gsub("USGS-", "", sites_near_5km$monitoring_location_id[58])
+paramflow <- "00060"     # water flow, cfs
+# East Branch Housatonic River at Coltsville, MA - USGS-01197000
+# Fetch daily water temperature -------------------------------------------
+flow <- read_waterdata_daily(
+  monitoring_location_id = paste("USGS", site, sep = "-"),
+  parameter_code = paramflow,
+  time = c(as.character(min(hor$SampleDate)), as.character(max(hor$SampleDate)))
+)
+
+# Clean USGS data
+flow_values <- flow %>%
+  st_drop_geometry() %>%
+  mutate(
+    time = as.Date(time),
+    Q_cm3s = value * 28316.8,   # cfs → cm³/s
+    width_cm = 1000,           # 10 m
+    depth_cm = 120,            # 1.2 m
+    area_cm2 = width_cm * depth_cm,
+    velocity_cms = Q_cm3s / area_cm2) %>%
+  select(time, Q_cm3s, velocity_cms)
+
+# Merge the water velocity
+hor <- hor %>%
+  left_join(flow_values %>% select(time, velocity_cms),
+            by = c("SampleDate" = "time"))
+
+# save
+write.csv(hor, "Data/HousatonicRiver/HousatonicRiver_env.csv", row.names = FALSE)
+
---
+++ R/ReadData/WaterFlow/USGSReadDataKalamazooRiver.R
@@ -0,0 +1,84 @@
+# -------------------------------------------------------------------
+# Code use to retrieve water flow (Q) from a USGS station
+
+# Install packages
+{
+  install.packages("dataRetrieval")
+  install.packages("dplyr")
+  install.packages('sf')
+  install.packages("zoo")
+  install.packages("lubridate")
+}
+
+# Load libraries
+{
+  library(dataRetrieval) # read data from USGS
+  library(dplyr)         # data manipulation
+  library(sf)            # for handling sf objects (used by read_waterdata_daily)
+  library(zoo)           # for interpolation
+  library(lubridate)
+  library(geosphere)
+}
+
+# Read data ---------------------------------------------------------------
+# From Data Folder, Kalamazoo River.csv
+kar <- read.csv("Data/Kalamazoo/KalamazooRiver_env.csv")
+
+# Convert from "m/d/YYYY" to Date
+kar$SampleDate <- as.Date(kar$SampleDate)
+
+# Seek stations
+sites <- read_waterdata_monitoring_location(
+  bbox = c(-85.79, 42.09, -85.39, 42.49))
+
+# replace with your target point (lon, lat)
+target_lon <- -85.5872
+target_lat <- 42.2917
+
+# extract coords from sf geometry
+coords <- st_coordinates(sites)
+sites_near_10km <- sites %>%
+  st_drop_geometry() %>%
+  mutate(
+    lon = coords[,1],
+    lat = coords[,2],
+    dist_km = distHaversine(
+      cbind(lon, lat),
+      cbind(target_lon, target_lat)
+    ) / 1000
+  ) %>%
+  filter(dist_km <= 10) %>%
+  arrange(dist_km) %>%
+  select(monitoring_location_id, monitoring_location_name, lat, lon, dist_km)
+
+# Define USGS site and parameter ------------------------------------------
+site <- gsub("USGS-", "", sites_near_10km$monitoring_location_id[81])
+paramflow <- "00060"     # water flow, cfs
+# Kalamazoo River at Comstock, MI - USGS-04106000
+# Fetch daily water temperature -------------------------------------------
+flow <- read_waterdata_daily(
+  monitoring_location_id = paste("USGS", site, sep = "-"),
+  parameter_code = paramflow,
+  time = c(as.character(min(kar$SampleDate)), as.character(max(kar$SampleDate)))
+)
+
+# Clean USGS data
+flow_values <- flow %>%
+  st_drop_geometry() %>%
+  mutate(
+    time = as.Date(time),
+    Q_cm3s = value * 28316.8 * 0.05,   # cfs → cm³/s, 0.05 is to get an estimate for Portage Creek at Kalamazoo, MI - USGS-04106500
+    width_cm = 1200,           # 12 m
+    depth_cm = 120,            # 1.2 m
+    area_cm2 = width_cm * depth_cm,
+    velocity_cms = Q_cm3s / area_cm2) %>%
+  select(time, Q_cm3s, velocity_cms)
+
+# Merge the water velocity
+kar <- kar %>%
+  left_join(flow_values %>% select(time, velocity_cms),
+            by = c("SampleDate" = "time"))
+
+# save
+write.csv(kar, "Data/Kalamazoo/KalamazooRiver_env.csv", row.names = FALSE)
+
