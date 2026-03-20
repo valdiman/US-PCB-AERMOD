32b6158cfd935eae1b7f374cd69026eae0c63e7b
Martinez Araneda <martie@uiowa.edu>
2026-03-20 11:58:46 -0500

    Renamed folder.

---
+++ R/DataProcessing/03DataAcquisitionUSGSWaterFlow/USGSReadDataSpokaneRiver.R
@@ -0,0 +1,84 @@
+# -------------------------------------------------------------------
+# Code use to retrieve water flow (Q) from a USGS station
+
+# Install packages
+{
+  install.packages("dataRetrieval")
+  install.packages("dplyr")
+  install.packages('sf')
+  install.packages("zoo")
+  install.packages("lubridate")
+}
+
+# Load libraries
+{
+  library(dataRetrieval) # read data from USGS
+  library(dplyr)         # data manipulation
+  library(sf)            # for handling sf objects (used by read_waterdata_daily)
+  library(zoo)           # for interpolation
+  library(lubridate)
+  library(geosphere)
+}
+
+# Read data ---------------------------------------------------------------
+# From Data Folder, Spokane River.csv
+spr <- read.csv("Data/Spokane/SpokaneRiverMeteoWaterTemp.csv")
+
+# Convert from "m/d/YYYY" to Date
+spr$SampleDate <- as.Date(spr$SampleDate)
+
+# Seek stations
+sites <- read_waterdata_monitoring_location(
+  bbox = c(-117.63, 47.46, -117.23, 47.86))
+
+# replace with your target point (lon, lat)
+target_lon <- -117.4260
+target_lat <- 47.6588
+
+# extract coords from sf geometry
+coords <- st_coordinates(sites)
+sites_near_5km <- sites %>%
+  st_drop_geometry() %>%
+  mutate(
+    lon = coords[,1],
+    lat = coords[,2],
+    dist_km = distHaversine(
+      cbind(lon, lat),
+      cbind(target_lon, target_lat)
+    ) / 1000
+  ) %>%
+  filter(dist_km <= 5) %>%
+  arrange(dist_km) %>%
+  select(monitoring_location_id, monitoring_location_name, lat, lon, dist_km)
+
+# Define USGS site and parameter ------------------------------------------
+site <- gsub("USGS-", "", sites_near_5km$monitoring_location_id[31])
+paramflow <- "00060"     # water flow, cfs
+# Spokane River at Spokane, WA - USGS-12422500 
+# Fetch daily water flow -------------------------------------------
+flow <- read_waterdata_daily(
+  monitoring_location_id = paste("USGS", site, sep = "-"),
+  parameter_code = paramflow,
+  time = c(as.character(min(spr$SampleDate)), as.character(max(spr$SampleDate)))
+)
+
+# Clean USGS data
+flow_values <- flow %>%
+  st_drop_geometry() %>%
+  mutate(
+    time = as.Date(time),
+    Q_cm3s = value * 28316.8,   # cfs → cm³/s
+    width_cm = 6000,           # 60 m
+    depth_cm = 200,            # 2 m
+    area_cm2 = width_cm * depth_cm,
+    velocity_cms = Q_cm3s / area_cm2) %>%
+  select(time, Q_cm3s, velocity_cms)
+
+# Merge the water velocity
+spr <- spr %>%
+  left_join(flow_values %>% select(time, velocity_cms),
+            by = c("SampleDate" = "time"))
+
+# save
+write.csv(spr, "Data/Spokane/SpokaneRiverMeteoWaterTempFlow.csv", row.names = FALSE)
+
---
+++ R/DataProcessing/PangaeaDownloadDataset.R
@@ -0,0 +1,140 @@
+# R code to download and format data from Pangaea
+# Select the locations
+
+# Packages and libraries needed -------------------------------------------------------------------
+# Install packages
+install.packages('pangaear')
+# Library
+library(pangaear) # Read data from Pangaea
+
+# Read data from Pangaea and format data ------------------------------
+
+# Read data from Pangaea repository
+# Citation:
+# Martinez, Andres (publication year): Dataset of surface water
+# concentrations of Polychlorinated Biphenyls in the U.S.
+# from 1979 - 2020 [dataset].  PANGAEA, https://doi.pangaea.de/10.1594/PANGAEA.972705
+# final doi: https://doi.org/10.1594/PANGAEA.972705
+
+# Set cache path to the project folder
+pg_cache$cache_path_set(full_path = "Data/")
+# Download original datasets from Pangaea
+d.0 <- pg_data(doi = '10.1594/PANGAEA.972705')
+
+# Extract dataset
+d <- d.0[[1]]$data
+
+# Need to change the columns name to they match the R codes
+# Vector with all the new column names provided
+new_col_names <- c(
+  "Source", "SampleID", "EPARegion", "StateSampled", "LocationName", 
+  "SiteName", "SiteID", "SampleDate", "Latitude", "Longitude", 
+  "PhaseMeasured", "EPAMethod", "AroclorCongener", "PCB1", "PCB2", "PCB3", 
+  "PCB4+10", "PCB5+8", "PCB6", "PCB7+9", "PCB11", "PCB12+13", "PCB14", 
+  "PCB15", "PCB16+32", "PCB17", "PCB18+30", "PCB19", 
+  "PCB20+21+28+31+33+50+53", "PCB22", "PCB23", "PCB24+27", "PCB25", 
+  "PCB26+29", "PCB34", "PCB35", "PCB36", "PCB37+42", "PCB38", "PCB39", 
+  "PCB40+41+64+71+72", "PCB43+49+52+69+73", "PCB44+47+65", "PCB45+51", 
+  "PCB46", "PCB48+59+62+75", "PCB54", "PCB55", "PCB56+60", "PCB57", 
+  "PCB58", "PCB61+66+70+74+76+93+95+98+100+102", "PCB63", "PCB67", 
+  "PCB68", "PCB77+85+110+111+115+116+117", "PCB78", "PCB79", "PCB80", 
+  "PCB81+86+87+97+107+108+109+112+119+124+125", 
+  "PCB82+135+144+151+154", "PCB83+99", "PCB84+92", "PCB88+91", "PCB89", 
+  "PCB90+101+113", "PCB94", "PCB96", "PCB103", "PCB104", "PCB105", 
+  "PCB106+118", "PCB114+122+131+133+142+146+165", "PCB120", "PCB121", 
+  "PCB123+139+140+147+149", "PCB126", "PCB127", "PCB128+162+166+167", 
+  "PCB129+137+138+158+160+163+164+176+178", "PCB130", 
+  "PCB132+153+161+168", "PCB134+143", "PCB136", "PCB141", "PCB145", 
+  "PCB148", "PCB150", "PCB152", "PCB155", "PCB156+157+172+197+200", 
+  "PCB159", "PCB169", "PCB170+190", "PCB171+173+202", "PCB174", 
+  "PCB175", "PCB177", "PCB179", "PCB180+193", "PCB181", 
+  "PCB182+187", "PCB183+185", "PCB184", "PCB186", "PCB188", "PCB189", 
+  "PCB191", "PCB192", "PCB194+205", "PCB195+208", "PCB196+203", 
+  "PCB198+199+201", "PCB204", "PCB206", "PCB207", "PCB209", "A1016", 
+  "A1221", "A1232", "A1242", "A1248", "A1254", "A1260", "tPCB"
+)
+
+# Apply the new column names
+colnames(d) <- new_col_names
+
+# See few rows and columns
+head(d)
+
+# Select Anacostia River data ---------------------------------------------
+anr <- d[d$LocationName == 'Anacostia River', ]
+# Remove metadata
+anr <- anr[ , !(names(anr) %in% c("Source", "SampleID", "EPARegion", "StateSampled",
+                               "PhaseMeasured", "EPAMethod", "AroclorCongener"))]
+# Remove non-reported values
+anr <- anr[, -c(7:117)]
+# Save data
+write.csv(anr, file = "Data/AnacostiaRiver/AnacostiaRiver.csv",
+          row.names = FALSE)
+
+# Select Fox River data ---------------------------------------------------
+fxr <- d[d$LocationName == 'Fox River', ]
+# Remove metadata
+fxr <- fxr[ , !(names(fxr) %in% c("Source", "SampleID", "EPARegion", "StateSampled",
+                               "PhaseMeasured", "EPAMethod", "AroclorCongener"))]
+# Remove Aroclor
+fxr <- fxr[ , -c(111:117)]
+# Save Fox River data
+write.csv(fxr, file = "Data/FoxRiver/FoxRiver.csv",
+          row.names = FALSE)
+
+# Select Housatonic River data ---------------------------------------------
+hor <- d[d$LocationName == 'Housatonic River', ]
+# Remove metadata
+hor <- hor[ , !(names(hor) %in% c("Source", "SampleID", "EPARegion", "StateSampled",
+                                  "PhaseMeasured", "EPAMethod", "AroclorCongener"))]
+# Remove non-reported values
+hor <- hor[ , -c(7:117)]
+# Save Fox River data
+write.csv(hor, file = "Data/HousatonicRiver/HousatonicRiver.csv",
+          row.names = FALSE)
+
+# Select Hudson River data ---------------------------------------------
+hur <- d[d$LocationName == 'Hudson River', ]
+# Remove metadata
+hur <- hur[ , !(names(hur) %in% c("Source", "SampleID", "EPARegion", "StateSampled",
+                                  "PhaseMeasured", "EPAMethod", "AroclorCongener"))]
+# Remove Aroclor
+hur <- hur[ , -c(111:117)]
+# Save Fox River data
+write.csv(hur, file = "Data/HudsonRiverAlbany/HudsonRiver.csv",
+          row.names = FALSE)
+
+# Select Kalamazoo River data ---------------------------------------------
+kar <- d[d$LocationName == 'Kalamazoo River', ]
+# Remove metadata
+kar <- kar[ , !(names(kar) %in% c("Source", "SampleID", "EPARegion", "StateSampled",
+                                  "PhaseMeasured", "EPAMethod", "AroclorCongener"))]
+# Remove non-reported values
+kar <- kar[ , -c(7:117)]
+# Save Fox River data
+write.csv(kar, file = "Data/Kalamazoo/KalamazooRiver.csv",
+          row.names = FALSE)
+
+# Select Passaic River data ---------------------------------------------
+par <- d[d$LocationName == 'Passaic River', ]
+# Remove metadata
+par <- par[ , !(names(par) %in% c("Source", "SampleID", "EPARegion", "StateSampled",
+                                  "PhaseMeasured", "EPAMethod", "AroclorCongener"))]
+# Remove Aroclor
+par <- par[ , -c(111:117)]
+# Save Fox River data
+write.csv(par, file = "Data/PassaicRiver/PassaicRiver.csv",
+          row.names = FALSE)
+
+# Select Spokane River data ---------------------------------------------
+spr <- d[d$LocationName == 'Spokane River', ]
+# Remove metadata
+spr <- spr[ , !(names(spr) %in% c("Source", "SampleID", "EPARegion", "StateSampled",
+                                  "PhaseMeasured", "EPAMethod", "AroclorCongener"))]
+# Remove Aroclor
+spr <- spr[ , -c(111:117)]
+# Save Fox River data
+write.csv(spr, file = "Data/Spokane/SpokaneRiver.csv",
+          row.names = FALSE)
+
+
